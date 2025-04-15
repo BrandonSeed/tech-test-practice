6d11f4c6f1b67d3debe4e662fd3c182716ad8922
BrandonSeed <brandon@seedshouse.co.uk>
2025-04-15 15:49:00 +0100

    add several more test cases

--- __tests__/longestSubstring.test.js
+++ __tests__/longestSubstring.test.js
@@ -1,10 +1,28 @@
-const longestSubsring = require('../questions/longestSubstring')
+const longestSubstring = require('../questions/longestSubstring')
 
-describe('longestSubstring', () => {
+describe('longestSubstring without repeat', () => {
     
-    test('should return length of 1 when only one letter is input', () => {
+    test('should return correct highest length when only one letter is input', () => {
         const testInput = 'a'
-        const actualOutput = longestSubsring(testInput)
+        const actualOutput = longestSubstring(testInput)
+        expect(actualOutput).toBe(1)
+    });
+
+    test('should return correct highest length when a mix of letters with repeats is input', () => {
+        const testInput = 'abcabcbb'
+        const actualOutput = longestSubstring(testInput)
+        expect(actualOutput).toBe(3)
+    });
+
+    test('should return correct highest length when a mix of letters with repeats and longest substring is after a repeat letter is input', () => {
+        const testInput = 'pwwkew'
+        const actualOutput = longestSubstring(testInput)
+        expect(actualOutput).toBe(3)
+    });
+
+    test('should retrun correct length when inly a repeated letter is input', () => {
+        const testInput = 'bbbb'
+        const actualOutput = longestSubstring(testInput)
         expect(actualOutput).toBe(1)
     });
 });
