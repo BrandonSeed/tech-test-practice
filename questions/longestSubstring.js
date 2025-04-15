12d4d5ba45365a2fb82553433eae01b854f87f38
BrandonSeed <brandon@seedshouse.co.uk>
2025-04-15 15:49:17 +0100

    complete functionality for current tests

--- questions/longestSubstring.js
+++ questions/longestSubstring.js
@@ -1,5 +1,19 @@
-function longestSubsring(str) {
-    return str.length
+function longestSubstring(str) {
+    let longestRunning = 0
+    let longest = 1
+    let usedLetters = ""
+    const strArray = str.split("")
+    strArray.forEach((letter) => {
+        if (usedLetters.includes(letter)) {
+            if (longestRunning > longest) longest = longestRunning
+            longestRunning = 1
+        }
+        else {
+            longestRunning ++
+            usedLetters = usedLetters + letter
+        }
+    });
+    return longest
 }
 
-module.exports = longestSubsring
+module.exports = longestSubstring
